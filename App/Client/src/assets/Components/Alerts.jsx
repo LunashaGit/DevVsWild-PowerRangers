26a423da2f8c0fd6b77583a7fa09a9f5d878fc4a
LunashaGit <lunamuylkens@gmail.com>
2023-03-24 22:55:10 +0100

    Alert Popup Button + Function

--- App/Client/src/assets/Components/Alerts.jsx
+++ App/Client/src/assets/Components/Alerts.jsx
@@ -1,3 +1,16 @@
-function Alerts() {}
+import { useState, useCallback } from "react";
+import AlertButton from "./AlertButton";
+function Alerts() {
+  const [showAlert, setShowAlert] = useState(false);
+  const toggleAlert = useCallback(
+    () => setShowAlert((showAlert) => !showAlert),
+    []
+  );
+  return (
+    <>
+      <AlertButton onClick={toggleAlert} />
+    </>
+  );
+}
 
 export default Alerts;
---
+++ App/Client/src/assets/Pages/Testing.jsx
@@ -0,0 +1,11 @@
+import Alerts from "../Components/Alerts";
+
+function Testing() {
+  return (
+    <div>
+      <Alerts />
+    </div>
+  );
+}
+
+export default Testing;
