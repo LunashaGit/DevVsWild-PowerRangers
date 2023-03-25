ba22050e1b7ea2adb227b55f77a6b44a035efa26
Lilou Mazzarisi <lisa_mazzarisi@hotmail.com>
2023-03-25 23:37:06 +0100

    style alert page

--- App/Client/src/assets/Components/AlertCancel.jsx
+++ App/Client/src/assets/Components/AlertCancel.jsx
@@ -1,14 +1,27 @@
 function AlertCancel(props) {
-  return (
-    <>
-      <button
-        className="absolute bottom-[25px] left-1/4 right-1/4"
-        onClick={props.onClick}
-      >
-        X
-      </button>
-    </>
-  );
+    return (
+        <>
+            <button
+                className="absolute w-min left-1/2 bottom-[25px] flex justify-center bg-orangeFox text-white p-2 rounded-full shadow-2xl"
+                onClick={props.onClick}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M6 18L18 6M6 6l12 12"
+                    />
+                </svg>
+            </button>
+        </>
+    )
 }
 
-export default AlertCancel;
+export default AlertCancel
--- App/Client/src/assets/Components/AlertsList.jsx
+++ App/Client/src/assets/Components/AlertsList.jsx
@@ -7,36 +7,42 @@ function AlertsList(props) {
             title: 'Poisonous plant',
             description:
                 'At this location, there are poisonous or irritating plants.',
+            url: '../src/assets/images/poison.png',
         },
         {
             id: 2,
             title: 'Slippery Path',
             description:
                 'At this location, the path can be slippery or dangerous to walk on.',
+            url: '../src/assets/images/slippery.png',
         },
         {
             id: 3,
             title: 'Blocked Path',
             description:
                 'At this location, the path stop and leads nowhere or might be a cliffs.',
+            url: '../src/assets/images/blocked.png',
         },
         {
             id: 4,
             title: 'Challenging Path',
             description:
                 'At this location, the path requires a climbing equipment (shoes spikes, climbing gears) ',
+            url: '../src/assets/images/challenging.png',
         },
         {
             id: 5,
             title: 'Dangerous Animals',
             description:
                 'At this location, you might encounter a wild animal. (black bear, mountain lions and rattlesnakes,… )',
+            url: '../src/assets/images/bear.png',
         },
         {
             id: 6,
             title: 'Dangerous Insects',
             description:
                 'At this location, you might encounter infectious insects. (hornet, mosquitos, wasp nest) ',
+            url: '../src/assets/images/wasp.png',
         },
     ]
 
@@ -46,12 +52,14 @@ function AlertsList(props) {
             title: 'Wild fires',
             description:
                 'ALERT: Severe wildfires in your area. We will send updates as conditions develop, please evacuate immediately. Please call the emergency number for assistance or check local media.',
+            url: '../src/assets/images/fire.png',
         },
         {
             id: 2,
             title: 'Zombie invasion  ',
             description:
                 'ALERT: Civil authorities in your area have reported that the bodies of the dead are returning to life and attacking your area. Follow the message on screen that will be updated as more information becomes available. Do not attempt to approach or apprehend these bodies, as they are considered extremely dangerous and life-threatening.',
+            url: '../src/assets/images/zombi.png',
         },
     ]
 
@@ -81,36 +89,38 @@ function AlertsList(props) {
     }
     return (
         <div
-            style={{
-                height: '100vh',
-                width: '100vw',
-            }}
-            className="alert alert-danger"
+            className="alert alert-danger absolute w-screen h-screen -top-[652px] -left-[132px] bg-white dark:bg-greyNight z-[6000] flex flex-col justify-items-center"
             role="alert"
         >
-            <ul className="list-group flex flex-wrap gap-4 justify-center left-1/4 right-1/4 absolute top-[100px]">
-                {Alerts.map((alert) => (
-                    <li
-                        className="list-group-item w-16 h-16 text-center text-white bg-black rounded-full"
-                        key={alert.id}
-                        onClick={() => AlertAPI(alert)}
-                    >
-                        <h3>{alert.title}</h3>
-                    </li>
-                ))}
-            </ul>
-
-            <ul className="list-group flex flex-wrap gap-4 justify-center left-1/4 right-1/4 absolute top-[250px]">
-                {Signals.map((Signal) => (
-                    <li
-                        className="list-group-item w-16 h-16 text-center text-white bg-black rounded-full"
-                        key={Signal.id}
-                        onClick={() => SignalsAPI(Signal)}
-                    >
-                        <h3>{Signal.title}</h3>
-                    </li>
-                ))}
-            </ul>
+            <h2 className="text-blackNero dark:text-white text-xl font-thin text-center m-14">
+                Send your alert
+            </h2>
+            <div className="flex flex-col gap-16">
+                <ul className="list-group flex flex-wrap gap-x-4 gap-y-16 justify-center">
+                    {Signals.map((Signal) => (
+                        <li
+                            className="list-group-item w-20 h-20 text-center text-white bg-black rounded-full"
+                            key={Signal.id}
+                            onClick={() => SignalsAPI(Signal)}
+                        >
+                            <img src={Signal.url} />
+                            <p className="text-[10px] pt-2">{Signal.title}</p>
+                        </li>
+                    ))}
+                </ul>
+                <ul className="list-group flex flex-wrap gap-x-4 gap-y-16 justify-center left-1/4 right-1/4">
+                    {Alerts.map((alert) => (
+                        <li
+                            className="list-group-item w-20 h-20 text-center text-white bg-black rounded-full"
+                            key={alert.id}
+                            onClick={() => AlertAPI(alert)}
+                        >
+                            <img src={alert.url} />
+                            <p className="text-[10px] pt-2">{alert.title}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
             <AlertCancel onClick={props.onClick} />
         </div>
