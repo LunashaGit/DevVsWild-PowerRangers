8e493c6f497dd398f514803eb38e3851479489e0
Lilou Mazzarisi <lisa_mazzarisi@hotmail.com>
2023-03-25 21:27:08 +0100

    css alerts/ signals

--- App/Client/src/assets/Components/AlertsList.jsx
+++ App/Client/src/assets/Components/AlertsList.jsx
@@ -1,60 +1,60 @@
-import AlertCancel from "./AlertCancel";
-import axios from "axios";
+import AlertCancel from './AlertCancel'
+import axios from 'axios'
 function AlertsList(props) {
-  const Alerts = [
-    {
-      id: 1,
-      title: "Alert 1",
-      description: "This is the first alert",
-    },
-    {
-      id: 2,
-      title: "Alert 2",
-      description: "This is the second alert",
-    },
-    {
-      id: 3,
-      title: "Alert 3",
-      description: "This is the third alert",
-    },
-  ];
+    const Alerts = [
+        {
+            id: 1,
+            title: 'Alert 1',
+            description: 'This is the first alert',
+        },
+        {
+            id: 2,
+            title: 'Alert 2',
+            description: 'This is the second alert',
+        },
+        {
+            id: 3,
+            title: 'Alert 3',
+            description: 'This is the third alert',
+        },
+    ]
 
-  const AlertAPI = async (e) => {
-    console.log(e);
-    const response = await axios
-      .post("http://127.0.0.1:8080/api/alerts", {
-        title: e.title,
-        description: e.description,
-        lon: props.startCoords.lon,
-        lat: props.startCoords.lat,
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  };
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-      }}
-      className="alert alert-danger"
-      role="alert"
-    >
-      <ul className="list-group flex flex-wrap gap-4 justify-center left-1/4 right-1/4 absolute top-[100px]">
-        {Alerts.map((alert) => (
-          <li
-            className="list-group-item w-16 h-16 text-center text-white bg-black rounded-full"
-            key={alert.id}
-            onClick={() => AlertAPI(alert)}
-          >
-            <h3>{alert.title}</h3>
-          </li>
-        ))}
-      </ul>
-      <AlertCancel onClick={props.onClick} />
-    </div>
-  );
+    const AlertAPI = async (e) => {
+        console.log(e)
+        const response = await axios
+            .post('http://127.0.0.1:8080/api/alerts', {
+                title: e.title,
+                description: e.description,
+                lon: props.startCoords.lon,
+                lat: props.startCoords.lat,
+            })
+            .then((res) => {
+                console.log(res)
+            })
+    }
+    return (
+        <div
+            style={{
+                height: '100vh',
+                width: '100vw',
+            }}
+            className="alert alert-danger"
+            role="alert"
+        >
+            <ul className="list-group flex flex-wrap gap-4 justify-center left-1/4 right-1/4 absolute top-[100px]">
+                {Alerts.map((alert) => (
+                    <li
+                        className="list-group-item w-16 h-16 text-center text-white bg-black rounded-full"
+                        key={alert.id}
+                        onClick={() => AlertAPI(alert)}
+                    >
+                        <h3>{alert.title}</h3>
+                    </li>
+                ))}
+            </ul>
+            <AlertCancel onClick={props.onClick} />
+        </div>
+    )
 }
 
-export default AlertsList;
+export default AlertsList
--- App/Client/src/assets/Components/SignalsList.jsx
+++ App/Client/src/assets/Components/SignalsList.jsx
@@ -1,59 +1,59 @@
-import SignalCancel from "./SignalsCancel";
-import axios from "axios";
+import SignalCancel from './SignalsCancel'
+import axios from 'axios'
 function SignalList(props) {
-  const Signals = [
-    {
-      id: 1,
-      title: "AlertOne",
-      description: "This is the first alert",
-    },
-    {
-      id: 2,
-      title: "AlertTwo",
-      description: "This is the second alert",
-    },
-    {
-      id: 3,
-      title: "AlertThree",
-      description: "This is the third alert",
-    },
-  ];
+    const Signals = [
+        {
+            id: 1,
+            title: 'AlertOne',
+            description: 'This is the first alert',
+        },
+        {
+            id: 2,
+            title: 'AlertTwo',
+            description: 'This is the second alert',
+        },
+        {
+            id: 3,
+            title: 'AlertThree',
+            description: 'This is the third alert',
+        },
+    ]
 
-  const SignalAPI = async (e) => {
-    console.log(e);
-    const response = await axios
-      .post("http://127.0.0.1:8080/api/signals", {
-        type: e.title,
-        lon: props.startCoords.lon,
-        lat: props.startCoords.lat,
-      })
-      .then((res) => {
-        console.log(res);
-      });
-  };
-  return (
-    <div
-      style={{
-        height: "100vh",
-        width: "100vw",
-      }}
-      className="alert alert-Signal"
-      role="alert"
-    >
-      <ul className="list-group flex flex-wrap gap-4 justify-center left-1/4 right-1/4 absolute top-[100px]">
-        {Signals.map((Signal) => (
-          <li
-            className="list-group-item w-16 h-16 text-center text-white bg-black rounded-full"
-            key={Signal.id}
-            onClick={() => SignalAPI(Signal)}
-          >
-            <h3>{Signal.title}</h3>
-          </li>
-        ))}
-      </ul>
-      <SignalCancel onClick={props.onClick} />
-    </div>
-  );
+    const SignalAPI = async (e) => {
+        console.log(e)
+        const response = await axios
+            .post('http://127.0.0.1:8080/api/signals', {
+                type: e.title,
+                lon: props.startCoords.lon,
+                lat: props.startCoords.lat,
+            })
+            .then((res) => {
+                console.log(res)
+            })
+    }
+    return (
+        <div
+            style={{
+                height: '100vh',
+                width: '100vw',
+            }}
+            className="alert alert-Signal"
+            role="alert"
+        >
+            <ul className="list-group flex flex-wrap gap-4 justify-center left-1/4 right-1/4 absolute top-[100px]">
+                {Signals.map((Signal) => (
+                    <li
+                        className="list-group-item w-16 h-16 text-center text-white bg-black rounded-full"
+                        key={Signal.id}
+                        onClick={() => SignalAPI(Signal)}
+                    >
+                        <h3>{Signal.title}</h3>
+                    </li>
+                ))}
+            </ul>
+            <SignalCancel onClick={props.onClick} />
+        </div>
+    )
 }
 
-export default SignalList;
+export default SignalList
