d923b62a8e92b90ce75feef32206bae241984e6d
TreshMiralissa <muratet.jade@yahoo.com>
2023-03-25 16:29:18 +0100

    button shadow

--- App/Client/src/assets/Components/AlertButton.jsx
+++ App/Client/src/assets/Components/AlertButton.jsx
@@ -3,7 +3,7 @@ function AlertButton(props) {
     <button
       className="rounded-full w-14 h-14 bg-orangeFox"
       onClick={props.onClick}
-    ><img src="../src/assets/images/warning.png" className="w-9 ml-2.5 mb-1.5"/>
+    ><img src="../src/assets/images/warning.png" className="w-9 ml-2.5 mb-1.5 shadow-lg"/>
     </button>
   );
 }
--- App/Client/src/assets/Components/DangerButton.jsx
+++ App/Client/src/assets/Components/DangerButton.jsx
@@ -1,7 +1,7 @@
 function AlertButton(props) {
   return (
     <button
-    className="rounded-full w-14 h-14 bg-orangeFox font-bold text-white text-xl"
+    className="rounded-full w-14 h-14 bg-orangeFox font-bold text-white text-xl shadow-lg"
       onClick={props.onClick}
     >SOS
     </button>
