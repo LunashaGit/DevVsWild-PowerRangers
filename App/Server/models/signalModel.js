4891e1d896c6ab41f334bccf9b117f119e0474aa
LunashaGit <lunamuylkens@gmail.com>
2023-03-25 11:21:40 +0100

    Fix Expire in Signal Model

--- App/Server/models/signalModel.js
+++ App/Server/models/signalModel.js
@@ -1,25 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const signalSchema = new Schema({
-    type: {
-        type: String,
-        required: true
-    },
-    lon: {
-        type: Number,
-        required: true
-    },
-    lat: {
-        type: Number,
-        required: true
-    },
-    expireAt: {
-        type: Date,
-        default: new Date(),
-        expires: 5
-    }
+  type: {
+    type: String,
+    required: true,
+  },
+  lon: {
+    type: Number,
+    required: true,
+  },
+  lat: {
+    type: Number,
+    required: true,
+  },
+  // expire
+  expireAt: {
+    type: Date,
+    default: Date.now,
+    index: { expires: 1 / 1000 },
+  },
 });
 
-module.exports = mongoose.model('Signal', signalSchema);
+signalSchema.index({ expireAt: "1s" }, { expireAfterSeconds: 0 });
+
+const Signal = mongoose.model("Signal", signalSchema);
+
+module.exports = Signal;
