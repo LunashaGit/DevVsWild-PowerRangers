597581ac68758f49022d2a00f61efe8dafea19f2
sachajeunejean <sacha.jeunejean@gmail.com>
2023-03-25 15:55:39 +0100

    Loading Page added

--- App/Client/src/assets/Pages/Homepage.jsx
+++ App/Client/src/assets/Pages/Homepage.jsx
@@ -5,6 +5,7 @@ import SearchBarForm from "../Components/SearchBarForm";
 import Alerts from "../Components/Alerts";
 import Danger from "../Components/Danger";
 import Map from "../Components/Map";
+import Loading from "../Components/Loading.jsx";
 
 export default function Homepage() {
   const [startPoint, setStartPoint] = useState(null);
@@ -34,10 +35,10 @@ export default function Homepage() {
   }, []);
 
   useEffect(() => {
-      fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&apiKey=a203d55a7a1f46cda1aef5ce6655c14c`)
-          .then(response => response.json())
-          .then(data => setStartPoint(data.features[0].properties.address_line1))
     navigator.geolocation.getCurrentPosition(function (position) {
+        fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${position.coords.latitude}&lon=${position.coords.longitude}&apiKey=a203d55a7a1f46cda1aef5ce6655c14c`)
+            .then(response => response.json())
+            .then(data => setStartPoint(data.features[0].properties.address_line1))
       setStartCoords({
         lat: position.coords.latitude,
         lon: position.coords.longitude,
@@ -45,20 +46,28 @@ export default function Homepage() {
     });
   }, []);
 
-  return (
-    <div className="Homepage relative flex items-center justify-center">
-      <div className="absolute top-5 flex justify-center left-1/4 right-1/4 z-[5000]">
-        {!showSearchBar && <SearchBarButton onClick={toggleSearchBar} />}
-        {showSearchBar && <SearchBarForm onSubmit={handleSubmit} startPoint={startPoint} />}
-      </div>
-      <div className="absolute bottom-5 z-[5000] flex justify-between gap-24">
-        <Danger />
-        <Alerts />
-      </div>
-        {startCoords && <Map
-            startCoords={startCoords}
-            endCoords={endCoords}
-        />}
-    </div>
-  );
+  if (!startCoords) {
+      return (
+          <>
+            <Loading />
+          </>
+      )
+  } else {
+      return (
+          <div className="Homepage relative flex items-center justify-center">
+              <div className="absolute top-5 flex justify-center left-1/4 right-1/4 z-[5000]">
+                  {!showSearchBar && <SearchBarButton onClick={toggleSearchBar}/>}
+                  {showSearchBar && <SearchBarForm onSubmit={handleSubmit} startPoint={startPoint}/>}
+              </div>
+              <div className="absolute bottom-5 z-[5000] flex justify-between gap-24">
+                  <Danger/>
+                  <Alerts/>
+              </div>
+              {startCoords && <Map
+                  startCoords={startCoords}
+                  endCoords={endCoords}
+              />}
+          </div>
+      );
+  }
 }
