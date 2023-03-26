a82f8adeadbfd996297a226f059ef7822fd9b338
LunashaGit <lunamuylkens@gmail.com>
2023-03-26 09:41:55 +0200

    Fix Images Popup

--- App/Client/src/assets/Components/Card.jsx
+++ App/Client/src/assets/Components/Card.jsx
@@ -7,27 +7,27 @@ function Card(props) {
   let Name;
 
   switch (props.valueAlert.type || props.valueAlert.title) {
-    case "animal":
+    case "Dangerous Animals":
       imgName = "./src/assets/images/bear.png";
       Name = "Dangerous Animals";
       break;
-    case "poisonous":
+    case "Poisonous plant":
       imgName = "./src/assets/images/poison.png";
       Name = "Poisonous plant";
       break;
-    case "snow":
+    case "Challenging Path":
       imgName = "./src/assets/images/challenging.png";
       Name = "Challenging Path";
       break;
-    case "block":
+    case "Blocked Path":
       imgName = "./src/assets/images/blocked.png";
       Name = "Blocked Path";
       break;
-    case "slippery":
+    case "Slippery Path":
       imgName = "./src/assets/images/slippery.png";
       Name = "Slippery Path";
       break;
-    case "insect":
+    case "Dangerous Insects":
       imgName = "./src/assets/images/wasp.png";
       Name = "Dangerous Insects";
       break;
@@ -54,13 +54,18 @@ function Card(props) {
 
   return (
     <div className="flex items-center justify-between h-20 text-center z-[2] rounded-xl w-[90vw] bg-zinc-800 bg-opacity-60">
-      <img src={`./src/assets/images/${imgName}`} />
+      <img src={imgName} />
       <div className="text-white">
         <h1 className="font-bold">{Name}</h1>
         <p className="font-thin">Still relevant ?</p>
       </div>
       <div className="flex flex-col justify-between text-black h-20 font-bold">
-        <button className="bg-orangeFox rounded-2xl w-14 h-7" onClick={props.handleAlertRemove}>Yes</button>
+        <button
+          className="bg-orangeFox rounded-2xl w-14 h-7"
+          onClick={props.handleAlertRemove}
+        >
+          Yes
+        </button>
         <button
           className="bg-lightGray rounded-2xl w-14 h-7"
           onClick={(e) => {
--- App/Server/db/seeder.js
+++ App/Server/db/seeder.js
@@ -1,59 +1,64 @@
-
-const Signal = require('../models/signalModel');
+const Signal = require("../models/signalModel");
 const dotenv = require("dotenv");
 const MongoClient = require("mongodb").MongoClient;
 
-
 dotenv.config({
-    path: ".env",
+  path: ".env",
 });
 
 const USER = process.env.MONGO_USER;
 const PASSWORD = process.env.MONGO_PASSWORD;
 
+const client = new MongoClient(
+  `mongodb+srv://${USER}:${PASSWORD}@power-rangers.9psnhuv.mongodb.net/database`,
+  {
+    useNewUrlParser: true,
+  }
+);
 
-const client = new MongoClient(`mongodb+srv://${USER}:${PASSWORD}@power-rangers.9psnhuv.mongodb.net/database`, {
-    useNewUrlParser: true
-});
-
-
-const typeArray = ["poisonous", "slippery", "block", "snow", "animal", "insect"];
+const typeArray = [
+  "poisonous",
+  "slippery",
+  "block",
+  "snow",
+  "animal",
+  "insect",
+];
 
 function getRandom(min, max) {
-    return Math.random() * (max - min) + min
+  return Math.random() * (max - min) + min;
 }
 
 function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * (max - min) + min);
 }
 
 async function seedDB() {
-    try {
-        await client.connect();
-        console.log("Connected correctly to server");
+  try {
+    await client.connect();
+    console.log("Connected correctly to server");
 
-        const collection = client.db("database").collection("signals");
+    const collection = client.db("database").collection("signals");
 
-        collection.drop();
+    collection.drop();
 
-        const signalEntry = [];
+    const signalEntry = [];
 
-        for(let i = 0; i < 20; i++){
-            signalEntry.push(
-                new Signal({
-                type:`${typeArray[getRandomInt(0,5)]}`,
-                lon: getRandom(3.9691265701222145,3.9729994667246955),
-                lat: getRandom(50.44324126829621,50.44554678286317)
-            })
-            )
-        }
-
-        collection.insertMany(signalEntry);
-
-        console.log("Database seeded! :)");
-    } catch (err) {
-        console.log(err.stack);
+    for (let i = 0; i < 20; i++) {
+      signalEntry.push(
+        new Signal({
+          type: `${typeArray[getRandomInt(0, 5)]}`,
+          lon: getRandom(3.9691265701222145, 3.9729994667246955),
+          lat: getRandom(50.44324126829621, 50.44554678286317),
+        })
+      );
     }
+
+    collection.insertMany(signalEntry);
+
+    console.log("Database seeded! :)");
+  } catch (err) {
+    console.log(err.stack);
+  }
 }
 seedDB();
-
