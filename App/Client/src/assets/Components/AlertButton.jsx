6636847b6f16bba83d04a21079fc8c191c9f9b3c
Lilou Mazzarisi <lisa_mazzarisi@hotmail.com>
2023-03-26 12:59:55 +0200

    transition test - FAIL

--- App/Client/src/assets/Components/AlertButton.jsx
+++ App/Client/src/assets/Components/AlertButton.jsx
@@ -1,15 +1,15 @@
 function AlertButton(props) {
-  return (
-    <button
-      className="rounded-full w-14 h-14 bg-orangeFox text-white"
-      onClick={props.onClick}
-    >
-      <img
-        src="../src/assets/images/warning.png"
-        className="w-9 ml-2.5 mb-1.5 shadow-lg"
-      />
-    </button>
-  );
+    return (
+        <button
+            className="rounded-full w-14 h-14 bg-orangeFox text-white"
+            onClick={props.onClick}
+        >
+            <img
+                src="../src/assets/images/warning.png"
+                className="w-9 ml-2.5 mb-1.5 shadow-lg"
+            />
+        </button>
+    )
 }
 
-export default AlertButton;
+export default AlertButton
